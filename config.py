8ddf2f87848122312b56d3187acb2ff3507af005
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-24 19:09:47 +0530

    config.py

--- config.py
+++ config.py
@@ -40,7 +40,8 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+
 
 
 # ==============================
